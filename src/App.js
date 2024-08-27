040769909afc377428fe813e0273ac8dc554a924
ericgrosse <ericgrosse1@gmail.com>
2024-08-27 13:38:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Vykan12 was here
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
